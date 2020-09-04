f6082ebd0b81fc8c34e4fb4769ef490c5f1b96d0
Charles Karari <kararicw@yahoo.com>
2020-09-04 13:12:08 +0300

    add content to index.html and main.js

--- script/main.js
+++ script/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+  $("#design-image").click(function () {
+    $("#design-image").slideDown('1500').hide('1000');
+    $("#design").show('1500');
+  });
+  $("#design").click(function () {
+    $("#design").slideUp('1500');
+    $("#design-image").slideDown('1500');
+  });
+});
+$(document).ready(function () {
   $("#development-image").click(function () {
     $("#development-image").slideDown('1500').hide('1000');
     $("#development").show('1500');
@@ -8,16 +18,7 @@ $(document).ready(function () {
     $("#development-image").slideDown('1500');
   });
 });
-$(document).ready(function () {
-  $("#design-image").hover(function () {
-    $("#design-image").slideDown('1500').hide('1000');
-    $("#design-image").show('1500');
-  });
-  $("#design").click(function () {
-    $("#design").slideUp('1500');
-    $("#design-image").slideDown('1500');
-  });
-});
+
 $(document).ready(function () {
   $("#product-image").click(function () {
     $("#product-image").slideDown('1500').hide('1000');
